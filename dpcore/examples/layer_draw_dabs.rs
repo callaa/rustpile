e7ecbded1f47cc95fd1f24fda540ea6602facc4d
Calle Laakkonen <laakkonenc@gmail.com>
2020-01-12 11:24:04 +0200

    Fix draw dabs example

--- dpcore/examples/layer_draw_dabs.rs
+++ dpcore/examples/layer_draw_dabs.rs
@@ -1,4 +1,4 @@
-use dpcore::paint::{editlayer, Blendmode, BrushMask, Color, Layer};
+use dpcore::paint::{editlayer, Blendmode, BrushMask, ClassicBrushCache, Color, Layer};
 
 mod utils;
 
@@ -8,6 +8,8 @@ fn main() {
 
     let mut x = 10;
     let mut w = 1i32;
+
+    let mut cache = ClassicBrushCache::new();
 
     while x < layer.width() as i32 - w {
         let brush = BrushMask::new_square_pixel(w as u32, 1.0);
@@ -32,10 +34,10 @@ fn main() {
             Blendmode::Normal,
         );
 
-        let (bx, by, brush) = BrushMask::new_gimp_style(x as f32, 90.0, w as f32, 0.0, 1.0);
+        let (bx, by, brush) = BrushMask::new_gimp_style(x as f32, 90.0, w as f32, 0.0, 1.0, &mut cache);
         editlayer::draw_brush_dab(&mut layer, 0, bx, by, &brush, &black, Blendmode::Normal);
 
-        let (bx, by, brush) = BrushMask::new_gimp_style(x as f32, 120.0, w as f32, 1.0, 1.0);
+        let (bx, by, brush) = BrushMask::new_gimp_style(x as f32, 120.0, w as f32, 1.0, 1.0, &mut cache);
         editlayer::draw_brush_dab(&mut layer, 0, bx, by, &brush, &black, Blendmode::Normal);
 
         w += 1;
