0d6265e6ef9f1968f9f86fcbb817a4701f08eb1d
Calle Laakkonen <laakkonenc@gmail.com>
2020-01-31 21:50:58 +0200

    Implement more drawpile-cli command line options

--- drawpile-cli/src/main.rs
+++ drawpile-cli/src/main.rs
@@ -1,4 +1,4 @@
-use clap::{App, Arg};
+use clap::{value_t, App, Arg, Error as ClapError};
 use tracing::Level;
 use tracing_subscriber;
 
@@ -26,7 +26,34 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 ),
         )
         .subcommand(
-            App::new("render").arg(Arg::with_name("INPUT").help("Input file").required(true)),
+            App::new("render")
+                .arg(Arg::with_name("INPUT").help("Input file").required(true))
+                .arg(Arg::with_name("OUTPUT").help("Output file"))
+                .arg(
+                    Arg::with_name("every-msg")
+                        .long("every-msg")
+                        .takes_value(true)
+                        .help("Save image every n messages"),
+                )
+                .arg(
+                    Arg::with_name("every-up")
+                        .long("every-up")
+                        .takes_value(true)
+                        .help("Save image every n undopoints")
+                        .conflicts_with("every-msg"),
+                )
+                .arg(
+                    Arg::with_name("resize")
+                        .long("resize")
+                        .takes_value(true)
+                        .help("Resize canvas to this size (WxH)"),
+                )
+                .arg(
+                    Arg::with_name("same-size")
+                        .long("same-size")
+                        .conflicts_with("resize")
+                        .help("Resize subsequent images to the original size"),
+                ),
         )
         .get_matches();
 
@@ -43,6 +70,22 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         ("render", Some(m)) => {
             let opts = RenderOpts {
                 input_file: m.value_of("INPUT").unwrap(),
+                output_file: m.value_of("OUTPUT").unwrap_or(""),
+                output_every: m.value_of("every-msg").or(m.value_of("every-up")).map(|v| {
+                    match v.parse::<u32>() {
+                        Ok(val) => val,
+                        Err(_) => {
+                            ClapError::value_validation_auto(format!("{}: Not a number", v)).exit()
+                        }
+                    }
+                }),
+                every_up: m.value_of("every-up").is_some(),
+                resize: if m.is_present("resize") {
+                    Some(value_t!(m, "resize", Size).unwrap_or_else(|e| e.exit()))
+                } else {
+                    None
+                },
+                same_size: m.is_present("same-size"),
             };
 
             render_recording(&opts)
--- drawpile-cli/src/renderer/mod.rs
+++ drawpile-cli/src/renderer/mod.rs
@@ -1,18 +1,56 @@
 use dpcore::canvas::CanvasState;
 use dpcore::paint::color::*;
-use dpcore::protocol::{open_recording, Compatibility, Message, ReadMessage};
+use dpcore::protocol::message::{CommandMessage, Message};
+use dpcore::protocol::{open_recording, Compatibility, ReadMessage};
 
 use tracing::{info, warn};
 
 use std::error::Error;
 use std::fmt;
 use std::io;
+use std::num::ParseIntError;
+use std::str::FromStr;
 use std::time::{Duration, Instant};
 
 use image;
 
+#[derive(Clone, Copy, PartialEq)]
+pub struct Size(u32, u32);
+
+impl FromStr for Size {
+    type Err = ParseIntError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let x = s.find('x').unwrap_or(0);
+        let w = s[..x].parse::<u32>()?;
+        let h = s[x + 1..].parse::<u32>()?;
+        Ok(Size(w, h))
+    }
+}
+
 pub struct RenderOpts<'a> {
+    /// Name of input recording file
     pub input_file: &'a str,
+
+    /// Name of output image file (sequence number is inserted before the suffix)
+    pub output_file: &'a str,
+
+    /// How often to write the image
+    pub output_every: Option<u32>,
+
+    /// Whether to write a message or an undopoint
+    pub every_up: bool,
+
+    /// Resize images to this size
+    pub resize: Option<Size>,
+
+    /// Resize subsequent images to the original size
+    pub same_size: bool,
+}
+
+struct RenderState {
+    resize: Option<Size>,
+    same_size: bool,
+    image_num: u32,
 }
 
 #[derive(Debug)]
@@ -44,6 +82,13 @@ pub fn render_recording(opts: &RenderOpts) -> Result<(), Box<dyn std::error::Err
     let start = Instant::now();
     let mut canvas = CanvasState::new();
     let mut total_render_time = Duration::new(0, 0);
+    let mut total_save_time = Duration::new(0, 0);
+    let mut message_counter = 0;
+    let mut state = RenderState {
+        resize: opts.resize,
+        same_size: opts.same_size,
+        image_num: opts.output_every.map_or(0, |_| 1),
+    };
 
     loop {
         match reader.read_next() {
@@ -51,11 +96,25 @@ pub fn render_recording(opts: &RenderOpts) -> Result<(), Box<dyn std::error::Err
                 let now = Instant::now();
                 match &m {
                     Message::Command(c) => {
+                        match (opts.every_up, c) {
+                            (true, CommandMessage::UndoPoint(_)) | (false, _) => {
+                                message_counter += 1;
+                            }
+                            _ => (),
+                        }
+
                         canvas.receive_message(c);
                     }
                     _ => (),
                 }
                 total_render_time += now.elapsed();
+
+                if let Some(e) = opts.output_every {
+                    if message_counter >= e {
+                        message_counter = 0;
+                        total_save_time += save_canvas(&opts, &mut state, &canvas)?;
+                    }
+                }
             }
             ReadMessage::Invalid(msg) => {
                 warn!("Invalid message: {}", msg);
@@ -69,10 +128,8 @@ pub fn render_recording(opts: &RenderOpts) -> Result<(), Box<dyn std::error::Err
         }
     }
 
-    let now = Instant::now();
-    let (img, w, h) = canvas.layerstack().to_image();
-    save_image("test.png", &img, w, h)?;
-    let total_save_time = now.elapsed();
+    total_save_time += save_canvas(&opts, &mut state, &canvas)?;
+
     let total_time = start.elapsed();
 
     info!(
@@ -85,16 +142,65 @@ pub fn render_recording(opts: &RenderOpts) -> Result<(), Box<dyn std::error::Err
     Ok(())
 }
 
-fn save_image(filename: &str, image: &[Pixel], width: u32, height: u32) -> io::Result<()> {
-    assert_eq!(image.len(), width as usize * height as usize);
+fn save_canvas(
+    opts: &RenderOpts,
+    state: &mut RenderState,
+    canvas: &CanvasState,
+) -> io::Result<Duration> {
+    let now = Instant::now();
 
-    let mut rgba = Vec::<u8>::with_capacity(width as usize * height as usize * 4);
-    for px in image.iter() {
+    let filename = make_filename(opts, state.image_num);
+
+    let (img, w, h) = canvas.layerstack().to_image();
+    let mut rgba = Vec::<u8>::with_capacity(w as usize * h as usize * 4);
+    for px in img.iter() {
         rgba.push(px[RED_CHANNEL]);
         rgba.push(px[GREEN_CHANNEL]);
         rgba.push(px[BLUE_CHANNEL]);
         rgba.push(px[ALPHA_CHANNEL]);
     }
+    let mut ib = image::RgbaImage::from_raw(w, h, rgba).unwrap();
 
-    image::save_buffer(filename, &rgba, width, height, image::RGBA(8))
+    let size = Size(w, h);
+
+    if let Some(resize) = state.resize {
+        if size != resize {
+            ib = image::imageops::resize(&ib, resize.0, resize.1, image::FilterType::CatmullRom);
+        }
+    } else if state.same_size {
+        state.resize = Some(size);
+    }
+
+    ib.save(&filename)?;
+    info!("Saved {}", filename);
+
+    state.image_num += 1;
+    Ok(now.elapsed())
+}
+
+fn make_filename(opts: &RenderOpts, index: u32) -> String {
+    if opts.output_file == "" {
+        let end = opts.input_file.rfind('.').unwrap_or(opts.input_file.len());
+
+        if index != 0 {
+            format!("{}-{}.png", &opts.input_file[..end], index)
+        } else {
+            format!("{}.png", &opts.input_file[..end])
+        }
+    } else {
+        if index != 0 {
+            let suffix = opts
+                .output_file
+                .rfind('.')
+                .unwrap_or(opts.output_file.len());
+            format!(
+                "{}-{}{}",
+                &opts.input_file[..suffix],
+                index,
+                &opts.output_file[suffix..]
+            )
+        } else {
+            opts.output_file.to_string()
+        }
+    }
 }
